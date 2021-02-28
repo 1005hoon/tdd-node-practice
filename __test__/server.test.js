f5f95fc291267ab43b04623e2ed7e5bdb273f8df
Hoon Oh <shoh@help-me.kr>
2021-02-28 22:20:44 +0900

    add bootcamps to server

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,10 +1,53 @@
 const app = require("../app");
 const request = require("supertest");
 
-describe("GET /", () => {
-  test('responds with message "Server Running"', async (done) => {
-    const { body } = await request(app).get("/");
-    expect(body.message).toBe("Server Running");
+describe("GET /api/v1/bootcamps", () => {
+  test('responds with 200 status code and message "Show all bootcamps"', async (done) => {
+    const { statusCode, body } = await request(app).get("/api/v1/bootcamps");
+    expect(statusCode).toBe(200);
+    expect(body.message).toBe("Show all bootcamps");
+    done();
+  });
+});
+
+describe("POST /api/v1/bootcamps", () => {
+  test('responds with 201 status code and message "Create new bootcamp"', async (done) => {
+    const { statusCode, body } = await request(app).post("/api/v1/bootcamps");
+    expect(statusCode).toBe(201);
+    expect(body.message).toBe("Create new bootcamp");
+    done();
+  });
+});
+
+describe("GET /api/v1/bootcamps/123123", () => {
+  test('responds with 200 status code and message "Show bootcamp with id 123123"', async (done) => {
+    const { statusCode, body } = await request(app).get(
+      "/api/v1/bootcamps/123123"
+    );
+    expect(statusCode).toBe(200);
+    expect(body.message).toBe("Show bootcamp with id 123123");
+    done();
+  });
+});
+
+describe("PUT /api/v1/bootcamps/123123", () => {
+  test('responds with 200 status code and message "Update bootcamp with id 123123"', async (done) => {
+    const { statusCode, body } = await request(app).put(
+      "/api/v1/bootcamps/123123"
+    );
+    expect(statusCode).toBe(200);
+    expect(body.message).toBe("Update bootcamp with id 123123");
+    done();
+  });
+});
+
+describe("DELETE /api/v1/bootcamps/123123", () => {
+  test("responds with 204 status code", async (done) => {
+    const { statusCode, body } = await request(app).delete(
+      "/api/v1/bootcamps/123123"
+    );
+    expect(statusCode).toBe(200);
+    expect(body.message).toBe("Delete bootcamp with id 123123");
     done();
   });
 });
--- app.js
+++ app.js
@@ -5,8 +5,45 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ message: "Server Running" });
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Show all bootcamps",
+  });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(201).json({
+    success: true,
+    message: "Create new bootcamp",
+  });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  const bootcampId = req.params.id;
+
+  res.status(200).json({
+    success: true,
+    message: `Show bootcamp with id ${bootcampId}`,
+  });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  const bootcampId = req.params.id;
+
+  res.status(200).json({
+    success: true,
+    message: `Update bootcamp with id ${bootcampId}`,
+  });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  const bootcampId = req.params.id;
+
+  res.status(200).json({
+    success: true,
+    message: `Delete bootcamp with id ${bootcampId}`,
+  });
 });
 
 module.exports = app;
