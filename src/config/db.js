75496342d9bd5726df091e2b67be1fd3f72f1566
Hoon <shoh@help-me.kr>
2021-03-02 12:26:26 +0900

    remove db logging feature

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('tddApp', 'testuser', 'userpw', {
   dialect: 'sqlite',
   storage: './database.sqlite',
+  logging: false,
 });
 
 module.exports = sequelize;
