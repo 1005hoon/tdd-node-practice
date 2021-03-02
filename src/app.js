b008bd86f37c408e2bfd325c1bcf60bc4bc8706a
Hoon <shoh@help-me.kr>
2021-03-02 12:19:01 +0900

    add feature to save users to database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const User = require('./user/User');
 const app = express();
 
-app.post('/api/v1/users', (req, res) => {
+app.post('/api/v1/users', async (req, res) => {
+  const user = await User.create(req.body);
   res.status(201).json({
     success: true,
     message: 'User Created',
