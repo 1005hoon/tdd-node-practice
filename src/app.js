063cf88d7d557b33d5dbe18dc38d1f6a61944096
Hoon <shoh@help-me.kr>
2021-03-02 12:22:44 +0900

    add body parser

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const User = require('./user/User');
 const app = express();
 
+app.use(express.json());
+
 app.post('/api/v1/users', async (req, res) => {
   const user = await User.create(req.body);
   res.status(201).json({
