a8e3a5ca6d34b924c7406f156f8c24252ecd94ce
Hoon <shoh@help-me.kr>
2021-03-02 12:26:12 +0900

    initialize connection to server

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const app = require('./src/app');
+const sequelize = require('./src/config/db');
+
+sequelize.sync();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
