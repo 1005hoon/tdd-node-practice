9e717ba80d0fdd3fc51c2e7f52d4a1c41ca87ee6
Hoon <shoh@help-me.kr>
2021-03-02 14:22:11 +0900

    add validation for user creation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,11 +3,18 @@ const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res, next) => {
   const hash = await bcrypt.hash(req.body.password, 10);
-
-  const user = await User.create({
+  const user = {
     ...req.body,
     password: hash,
-  });
+  };
+
+  if (user.username === null) {
+    return res.status(400).json({
+      success: false,
+      message: 'empty username',
+    });
+  }
+  const newUser = await User.create(user);
 
   res.status(201).json({
     success: true,
