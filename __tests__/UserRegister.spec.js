756fae3a2b38a4d4afa5647ff1ac29aa843bdaec
Hoon <shoh@help-me.kr>
2021-03-02 14:00:45 +0900

    refactor testing code

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -14,68 +14,50 @@ beforeEach(() => {
 });
 
 describe('User Registration', () => {
-  test('returns 200 OK when signup request is valid', async (done) => {
-    const { statusCode } = await request(app).post('/api/v1/users').send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'user123123',
+  const createValidUser = async (username = 'user1', email = 'user1@mail.com', password = 'user123123') => {
+    return await request(app).post('/api/v1/users').send({
+      username,
+      email,
+      password,
     });
+  };
+
+  test('returns 201 Created when signup request is valid', async () => {
+    const { statusCode } = await createValidUser();
     expect(statusCode).toBe(201);
-    done();
   });
 
-  test('returns success message when signup request is valid', async (done) => {
-    const { body } = await request(app).post('/api/v1/users').send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'user123123',
-    });
+  test('returns success message when signup request is valid', async () => {
+    const { body } = await createValidUser();
     expect(body.message).toBe('User Created');
-    done();
   });
 
-  test('saves the user to database', async (done) => {
+  test('saves the user to database', async () => {
     // save user
-    await request(app).post('/api/v1/users').send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'user123123',
-    });
+    await createValidUser();
 
     // query to see if user is saved
     const users = await User.findAll();
     expect(users.length).toBe(1);
-    done();
   });
 
-  test('saves the username and email to database', async (done) => {
+  test('saves the username and email to database', async () => {
     // save user
-    await request(app).post('/api/v1/users').send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'user123123',
-    });
+    await createValidUser();
 
     // query to see if user is saved
     const users = await User.findAll();
     const user = users[0];
     expect(user.username).toBe('user1');
     expect(user.email).toBe('user1@mail.com');
-    done();
   });
 
-  test('hashes the password in database', async (done) => {
-    await request(app).post('/api/v1/users').send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'user123123',
-    });
-
+  test('hashes the password in database', async () => {
+    await createValidUser();
     const users = await User.findAll();
     const { username, email, password } = users[0];
     expect(username).toBe('user1');
     expect(email).toBe('user1@mail.com');
     expect(password).not.toBe('user123123');
-    done();
   });
 });
--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -2,6 +2,6 @@ const express = require('express');
 const { createUser } = require('../controllers/userController');
 const router = express.Router();
 
-router.post('/', createUser);
+router.route('/').post(createUser);
 
 module.exports = router;
